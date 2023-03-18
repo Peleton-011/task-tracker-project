fca8ab3f8dafe411b7e40e9037b592f419b87f59
Nico <nicodiazan@gmail.com>
2023-03-18 17:36:36 +0100

    Add skeleton for InputField and AddTask components.

---
+++ src/components/AddTask.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputField from './InputField'
+
+const AddTask = () => {
+  return (
+    <form className="add-form">
+
+    </form>
+  )
+}
+
+export default AddTask

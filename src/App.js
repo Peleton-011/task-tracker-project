2f610b0660da82b426d125b9ce5f43776ed1ddcf
Nico <nicodiazan@gmail.com>
2023-03-18 16:52:24 +0100

    Add a task list to read from for testing purposes.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import Tasks from "./components/Tasks";
 import { useState } from "react";
 
 function App() {
-    const [tasks, setTasks] = useState([{}]);
+    const [tasks, setTasks] = useState([
+      {
+        id: 1,
+        text: "Do the dishes",
+        day: "Feb 4th at 12pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Do the laundry",
+        day: "Mar 7th at 2am",
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: "Do ur mom",
+        day: "Apr 20th at 4:20pm",
+        reminder: true,
+      }
+    
+    ]);
     return (
         <div className="container">
             <Header title="Task Tracker" />

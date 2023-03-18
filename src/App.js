eb344c169e8d7acb28f729ea83988b5eeac65c18
Nico <nicodiazan@gmail.com>
2023-03-18 16:34:09 +0100

    Reformat for readability and consistency.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="container">
-      <Header title="Task Tracker"/>
-    </div>
-  );
+    return (
+        <div className="container">
+            <Header title="Task Tracker" />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,27 @@
-import PropTypes from "prop-types"
-import React from "react"
+import PropTypes from "prop-types";
+import React from "react";
 
-import Button from "./Button"
+import Button from "./Button";
 
 const Header = ({ title }) => {
-const onClick = () => {
-  console.log("clicked")
-}
+    const onClick = () => {
+        console.log("clicked");
+    };
 
-  return (
-    <header>
-      <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
-    </header>
-  )
-}
+    return (
+        <header>
+            <h1>{title}</h1>
+            <Button color="green" text="Add" onClick={onClick} />
+        </header>
+    );
+};
 
 Header.defaultProps = {
-  title: "Task Tracker",
-}
+    title: "Task Tracker",
+};
 
 Header.propTypes = {
-  title: PropTypes.string.isRequired,
-}
+    title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;

6616e0ca982ebdd30cb021e54fbca3be27456244
Nico <nicodiazan@gmail.com>
2023-03-18 17:04:59 +0100

    Fix temporary output to the deleteTask() function.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,11 +3,15 @@ import PropTypes from "prop-types";
 
 import Task from "./Task";
 
-const Tasks = ({ tasks, setTasks, onDelete}) => {
+const Tasks = ({ tasks, setTasks, onDelete }) => {
     return (
         <>
             {tasks.map((task) => (
-                <Task key={task.id} task={task} onDelete={onDelete}/>
+                <Task
+                    key={task.id}
+                    task={task}
+                    onDelete={() => onDelete(task.id)}
+                />
             ))}
         </>
     );

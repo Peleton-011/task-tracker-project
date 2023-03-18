743ca9a7cc3d278480df0b54fa5154ed4fe4560e
Nico <nicodiazan@gmail.com>
2023-03-18 16:28:11 +0100

    Add props to Button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Button = ({color, text}) => {
-  return <button style={{backgroundColor: color}} className="btn">{text}</button>
+const Button = ({ color, text }) => {
+    return (
+        <button style={{ backgroundColor: color }} className="btn">
+            {text}
+        </button>
+    );
+};
+
+Button.defaultProps = {
+    color: "steelblue",
+};
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
 }
 
-export default Button
+export default Button;

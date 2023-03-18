9296cca45b2a05c90c8df34b4659ecd69385c8e9
Nico <nicodiazan@gmail.com>
2023-03-18 16:56:55 +0100

    Implement an individual Task component.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Task from "./Task";
+
 const Tasks = ({ tasks, setTasks }) => {
     return (
         <>
             {tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>
+                <Task key={task.id} task={task} />
             ))}
         </>
     );

18d66947675562830dc70468b4e60fa31df6bacd
Nico <nicodiazan@gmail.com>
2023-03-19 11:40:52 +0100

    Add multiple GenericInput components to AddTask.

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,7 +4,10 @@ import GenericInput from "./GenericInput";
 const AddTask = () => {
     return (
         <form className="add-task">
-            <GenericInput />
+            <GenericInput inputId="task-name" inputLabel="Task" inputType="text" inputPlaceholder="Add Task"/>
+            <GenericInput inputId="task-date" inputLabel="Day & Time" inputType="text" inputPlaceholder="Add Day & Time"/>
+            <GenericInput inputId="task-reminder" inputLabel="Set Reminder" inputType="checkbox" />
+
         </form>
     );
 };

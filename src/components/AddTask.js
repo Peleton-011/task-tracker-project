fb50f5738364f2e640b8637dda05878dcee06e7e
Nico <nicodiazan@gmail.com>
2023-03-19 11:46:48 +0100

    Add some styling to the submit button.

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -7,7 +7,7 @@ const AddTask = () => {
             <GenericInput inputId="task-name" inputLabel="Task" inputType="text" inputPlaceholder="Add Task"/>
             <GenericInput inputId="task-date" inputLabel="Day & Time" inputType="text" inputPlaceholder="Add Day & Time"/>
             <GenericInput inputClass="form-control-check" inputId="task-reminder" inputLabel="Set Reminder" inputType="checkbox" />
-            <input type="submit" value="Save Task"/>
+            <input className="btn btn-block" type="submit" value="Save Task"/>
         </form>
     );
 };

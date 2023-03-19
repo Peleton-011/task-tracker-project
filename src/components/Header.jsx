50adc6c964f4ef6b029e7f359828f3f45d935282
Nico <nicodiazan@gmail.com>
2023-03-19 17:57:10 +0100

    Minor reformat.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,11 @@ const Header = ({ title, onAdd, showAdd }) => {
     return (
         <header>
             <h1>{title}</h1>
-            <Button color={showAdd ? "red" : "green"} text={showAdd ? "Close" : "Add"} onClick={onAdd} />
+            <Button
+                color={showAdd ? "red" : "green"}
+                text={showAdd ? "Close" : "Add"}
+                onClick={onAdd}
+            />
         </header>
     );
 };

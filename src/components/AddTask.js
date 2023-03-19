c46d1dd2f7d9c340865efe76b0edaec717087ace
Nico <nicodiazan@gmail.com>
2023-03-19 11:50:18 +0100

    Add basis for state implementation.

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { useState } from "react";
+
 import GenericInput from "./GenericInput";
 
 const AddTask = () => {
+    const [name, setName] = useState("");
+    const [date, setDate] = useState("");
+    const [reminder, setReminder] = useState(false);
+
     return (
         <form className="add-form">
-            <GenericInput inputId="task-name" inputLabel="Task" inputType="text" inputPlaceholder="Add Task"/>
-            <GenericInput inputId="task-date" inputLabel="Day & Time" inputType="text" inputPlaceholder="Add Day & Time"/>
-            <GenericInput inputClass="form-control-check" inputId="task-reminder" inputLabel="Set Reminder" inputType="checkbox" />
-            <input className="btn btn-block" type="submit" value="Save Task"/>
+            <GenericInput
+                inputId="task-name"
+                inputLabel="Task"
+                inputType="text"
+                inputPlaceholder="Add Task"
+            />
+            <GenericInput
+                inputId="task-date"
+                inputLabel="Day & Time"
+                inputType="text"
+                inputPlaceholder="Add Day & Time"
+            />
+            <GenericInput
+                inputClass="form-control-check"
+                inputId="task-reminder"
+                inputLabel="Set Reminder"
+                inputType="checkbox"
+            />
+            <input className="btn btn-block" type="submit" value="Save Task" />
         </form>
     );
 };

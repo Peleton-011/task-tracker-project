1aa067896f2a62640d983e5a8e8d706f0f87a2c0
Nico <nicodiazan@gmail.com>
2023-03-19 11:26:16 +0100

    Add skeleton for the actual input.

--- src/components/GenericInput.js
+++ src/components/GenericInput.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const GenericInput = () => {
-    return <div className="form-control">Dummy Text</div>;
+    return (
+        <div className="form-control">
+            <label htmlFor=""></label>
+            <input type="text" />
+        </div>
+    );
 };
 
 export default GenericInput;

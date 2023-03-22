a9ac3fa406319357ef3d81d8eb14b93082334b17
Nico <nicodiazan@gmail.com>
2023-03-22 10:10:42 +0100

    Begin work on a modal AddTask form.

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 
 import GenericInput from "./GenericInput";
 
@@ -27,33 +28,45 @@ const AddTask = ({ onAdd, setShowAdd }) => {
     };
 
     return (
-        <form className="add-form" onSubmit={onSubmit}>
-            <GenericInput
-                inputId="task-name"
-                inputLabel="Task"
-                inputType="text"
-                inputPlaceholder="Add Task"
-                onChange={(e) => setName(e.target.value)}
-
-                required={true}
-            />
-            <GenericInput
-                inputId="task-date"
-                inputLabel="Day & Time"
-                inputType="text"
-                inputPlaceholder="Add Day & Time"
-                onChange={(e) => setDate(e.target.value)}
-            />
-            <GenericInput
-                inputClass="form-control-check"
-                inputId="task-reminder"
-                inputLabel="Set Reminder"
-                inputType="checkbox"
-                checked={reminder}
-                onChange={(e) => setReminder(e.currentTarget.checked)}
-            />
-            <input className="btn btn-block" type="submit" value="Save Task" />
-        </form>
+        <div className="modal">
+            <form className="add-form" onSubmit={onSubmit}>
+                <fieldset>
+                    <legend>New Task</legend>
+                    <FaTimes
+                        style={{ color: "red", cursor: "pointer" }}
+                        onClick={() => setShowAdd(false)}
+                    />
+                    <GenericInput
+                        inputId="task-name"
+                        inputLabel="Task"
+                        inputType="text"
+                        inputPlaceholder="Add Task Name"
+                        onChange={(e) => setName(e.target.value)}
+                        required={true}
+                    />
+                    <GenericInput
+                        inputId="task-date"
+                        inputLabel="Day & Time"
+                        inputType="text"
+                        inputPlaceholder="Add Day & Time"
+                        onChange={(e) => setDate(e.target.value)}
+                    />
+                    <GenericInput
+                        inputClass="form-control-check"
+                        inputId="task-reminder"
+                        inputLabel="Set Reminder"
+                        inputType="checkbox"
+                        checked={reminder}
+                        onChange={(e) => setReminder(e.currentTarget.checked)}
+                    />
+                    <input
+                        className="btn btn-block"
+                        type="submit"
+                        value="Save Task"
+                    />
+                </fieldset>
+            </form>
+        </div>
     );
 };
 
